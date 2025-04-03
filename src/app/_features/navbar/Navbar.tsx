f7ed357468e4f248f552b3dd167300409dd78dd9
FrancoisToureille <francois.toureille@etu.univ-amu.fr>
2025-04-03 14:40:54 +0200

    ajout de la premeiere nav bar

--- src/app/_features/navbar/Navbar.tsx
+++ src/app/_features/navbar/Navbar.tsx
@@ -1,5 +1,38 @@
+import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 
 export default function Navbar() {
-  return <Button variant='outline'>test</Button>;
+  return (
+    <nav className='bg-gray-800 text-white p-4 flex justify-between items-center'>
+      <div className='flex items-center space-x-2'>
+        <img
+          src='images/Epstein.jpg'
+          alt='Logo'
+          className='w-10 h-10 rounded-full'
+        />
+        <Link href='/' className='text-xl font-bold'>
+          PedObserver
+        </Link>
+      </div>
+
+      <div className='space-x-4'>
+        <Link href='/conversation' className='hover:underline'>
+          Prédateurs
+        </Link>
+        <Link href='/dashboard' className='hover:underline'>
+          Profils
+        </Link>
+        <Link href='/create-profile' className='hover:underline'>
+          Création de profil
+        </Link>
+      </div>
+
+      <Button
+        variant='outline'
+        className='border-white text-white hover:bg-gray-700'
+      >
+        Se connecter
+      </Button>
+    </nav>
+  );
 }
